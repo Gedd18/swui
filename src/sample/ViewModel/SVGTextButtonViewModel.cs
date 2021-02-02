ec25f8392ea52a619b425a8a3eb0ca24190ce853
Gedd18 <github.gedd18@outlook.com>
2021-02-02 16:17:32 +0100

    [Sample] Add SVGTextButton layout interactivity

--- src/sample/ViewModel/SVGTextButtonViewModel.cs
+++ src/sample/ViewModel/SVGTextButtonViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MvvmNext;
 using MvvmNext.CommandWpf;
+using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SWUI.Sample.ViewModel
 {
@@ -8,9 +10,30 @@ namespace SWUI.Sample.ViewModel
     {
         public RelayCommand SmileCommand { get; private set; }
 
+        public RelayCommand SetIconPositionLeft { get; private set; }
+
+        public RelayCommand SetIconPositionRight { get; private set; }
+
+        public RelayCommand SetIconPositionTop { get; private set; }
+
+        public RelayCommand SetIconPositionBottom { get; private set; }
+
+        public Dock Position { get; private set; }
+
         public SVGTextButtonViewModel()
         {
             SmileCommand = new RelayCommand(Smile);
+            SetIconPositionLeft = new RelayCommand(() => SetIconPosition(Dock.Left));
+            SetIconPositionRight = new RelayCommand(() => SetIconPosition(Dock.Right));
+            SetIconPositionTop = new RelayCommand(() => SetIconPosition(Dock.Top));
+            SetIconPositionBottom = new RelayCommand(() => SetIconPosition(Dock.Bottom));
+            SetIconPosition(Dock.Bottom);
+        }
+
+        private void SetIconPosition(Dock position)
+        {
+            Position = position;
+            RaisePropertyChanged(() => Position);
         }
 
         private void Smile()
